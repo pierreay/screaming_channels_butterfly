122f0312d55efc2c1bf47a0f92fbd77fc1dd9cb1
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2022-11-25 15:56:25 +0100

    Allow to select flashing device in script

--- compile_flash.sh
+++ compile_flash.sh
@@ -1,5 +1,9 @@
 #!/bin/zsh
-SERIAL_PORT="/dev/ttyACM0"
+if [[ $# == 1 ]]; then
+    SERIAL_PORT="$1"
+else
+    SERIAL_PORT="/dev/ttyACM0"
+fi
 echo "SERIAL_PORT=$SERIAL_PORT"
 whadup && read "?Please, press RESET button to put dongle in bootloader mode. Continue?" \
     && make && make send "SERIAL_PORT=$SERIAL_PORT"
